29925d3d3862db2c35e779a39fd7e236929e560d
ikra karaca <ikranuramine@hotmail.com>
2025-08-27 01:25:43 +0300

    Add project files.

---
+++ KitapOrnek/Kitap.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapOrnek
+{
+    internal class Kitap
+    {
+        private string _kitapAdi;
+        public string kitapAdi
+        {
+            get
+            {
+                return _kitapAdi;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("tanimsiz");
+                }
+                else
+                {
+                    _kitapAdi = value;
+                }
+            }
+        }
+
+        private string _yazarAdi;
+        public string yazarAdi
+        {
+            get
+            {
+                return _yazarAdi;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("tanimsiz");
+                }
+                else
+                {
+                    _yazarAdi = value;
+                }
+            }
+        }
+        private int _yil;
+        public int yil
+        {
+            get
+            {
+                return _yil;
+            }
+            set
+            {
+                if (value < 1900)
+                {
+                    value = 1900;
+                    _yil = value;
+                }
+                else if (value > 2100)
+                {
+                    value = 2100;
+                    _yil = value;
+                }
+                else
+                {
+                    _yil = value;
+                }
+
+
+            }
+        }
+        private int _fiyat;
+        public int fiyat
+        {
+            get
+            {
+                return _fiyat;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                    _fiyat = value;
+                }
+                else
+                {
+                    _fiyat = value;
+                }
+            }
+
+        }
+    }
+}
---
+++ KitapOrnek/Program.cs
@@ -0,0 +1,27 @@
+﻿using KitapOrnek;
+
+Kitap[] raf = new Kitap[3];
+for (int i = 0; i < raf.Length; i++)
+{
+    Console.WriteLine("Kitap Adi:");
+    string kitapAdi = Console.ReadLine();
+    Console.WriteLine("Yazar Adi:");
+    string yazarAdi = Console.ReadLine();
+    Console.WriteLine("Yil:");
+    int yil = int.Parse(Console.ReadLine());
+    Console.WriteLine("Fiyat giriniz: ");
+    int fiyat = int.Parse(Console.ReadLine()); 
+
+    Kitap kitap = new Kitap();
+    kitap.kitapAdi = kitapAdi;
+    kitap.yazarAdi = yazarAdi;
+    kitap.yil = yil;
+    kitap.fiyat = fiyat;
+
+    raf[i] = kitap; 
+}
+Console.WriteLine("Rafinizdaki kitaplar:");
+foreach(var kitap in raf)
+{
+    Console.WriteLine($"Kitap Adi: {kitap.kitapAdi}, Yazar Adi: {kitap.yazarAdi}, Yil: {kitap.yil}, Fiyat: {kitap.fiyat}");
+}
